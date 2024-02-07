a39defbb91c11f3cb37973e16b020c8bae620c1e
Tushar Konala <tusharkonala284@gmail.com>
2024-02-07 20:45:04 +0530

    Show a spinner while fetching the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -9,7 +9,10 @@ const GameCardContainer = ({ children }: Props) => {
   return (
     <Box borderRadius={10} overflow={"hidden"}>
       {/* we removed the "width" bcos we don't want the cards to have a fixed width
-      by default our width is set to 100% */}
+      by default our width is set to 100%
+      the "main" column contains these cards we have set its width to 1fr meaning
+      it will occupy the entire available space and we haven't specified the width 
+      of these cards hence all cards will distrubute the available space equally */}
       {children}
     </Box>
   );
--- src/components/GenreList.1.tsx
+++ src/components/GenreList.1.tsx
@@ -2,15 +2,10 @@ import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
-  const { data, isLoading, error } = useGenres();
+export const GenreList = () => {
+  const { data, isLoading } = useGenres();
   // we retrieved the property "isLoading"
-
-  if (error) return null;
-  // we dont want too many error messages hence returning null
   if (isLoading) return <Spinner />;
-  // if isloading is true we display a spinner ie we render buffer icon till the genre list
-  // is being retrieved from the server
 
   return (
     <List>
@@ -29,5 +24,3 @@ const GenreList = () => {
     </List>
   );
 };
-
-export default GenreList;
