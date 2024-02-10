a0adc9cdc5bab3bda1405ca2b8a68e0eb900a901
Tushar Konala <tusharkonala284@gmail.com>
2024-02-10 16:54:25 +0530

    Customize the theme

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,6 @@
 
 import genres from "../data/genres";
 
-// we don't want to send an additional request to fetch the genre list instaed we copied the response object in
-// genres.ts, hence we can render it without making an additonal request
-
 export interface Genre {
     id: number;
     name: string;
--- src/theme.ts
+++ src/theme.ts
@@ -1,33 +1,32 @@
 import { extendTheme, ThemeConfig} from "@chakra-ui/react";
-// we need to customize the default theme offered by chakra ui
-// extendTheme: This is a function provided by Chakra UI that allows you to extend and customize 
-// the default theme.
-// ThemeConfig: This is an interface in Chakra UI that defines the configuration options for the theme.
-//  In this case, you are using it to set the initialColorMode to 'dark'.
+
 
 const config: ThemeConfig = {
     initialColorMode: 'dark'
 }
-// const config: This declares a constant variable named config 
-// using the const keyword. The variable is going to hold the configuration object for the Chakra UI theme.
-// This specifies the type of the variable config. In TypeScript 
-// (or environments with type checking), it's explicitly stating that config should adhere
-//  to the ThemeConfig interface. 
-// The term "adhere" means to stick to, follow, or conform to a particular set of rules, guidelines, standards,
-//  or expectations. In the context of programming and type systems, when we say that a variable adheres to a
-//   certain type or interface, it means
-//  that the variable's structure and content align with the specifications defined by that type or interface.
-// { initialColorMode: 'dark' }: This is an object that sets a specific configuration option:
-// initialColorMode: This option sets the initial color mode for the theme. In this case, it is set to 'dark',
-//  indicating that the default color mode when the application starts will be the dark color scheme.
 
-const theme = extendTheme({config});
-// extendTheme: This is a function provided by Chakra UI that allows you to extend and customize the default theme.
-//  It takes an object as an argument, where you can specify various customizations for the theme.
+
+const theme = extendTheme({config,
+colors: {
+    gray: {
+        50: '#f9f9f9',
+        100: '#ededed',
+        200: '#d3d3d3',
+        300: '#b3b3b3',
+        400: '#a0a0a0',
+        500: '#898989',
+        600: '#6c6c6c',
+        700: '#202020',
+        800: '#121212',
+        900: '#111'
+    }
+}});
+
+// we are customizing the theme, we are mapping different shades of grey(50,100,etc) to our preferred shades
+// for eg: 50 is assigned the value #f9f9f9 (gray is an object with keys ie properties like
+// 50,100, etc whic represents its different shades)
+
 
 export default theme;
 
-// to make sure that this is working, go to chrome dev tools then go to "applications" then go to local storage
-// and delete the key having value "light" 
-// we do this because chakra ui stores the mode in the local storage so that it can remember it in the future
-// so "light" mode was already stored ie before applying these changes
+
